b1ae2f822cd7f49dbb54424adee42cf58b206d41
Peter <peter.w@droidtown.co>
2023-06-29 22:06:07 +0800

    Fix a werid regex bug. (I don't think that our fault.)

--- tools/person_classifier.py
+++ tools/person_classifier.py
@@ -5,9 +5,11 @@ import json
 import os
 import re
 
-personPatLIST = ["{}\s?（[^名又]*）",
-                 "{}\s?([^名又]*)",
-                 "{}，[^，]+人"
+from pprint import pprint
+
+personPatLIST = ["^{}\s?（[^名又a-zA-Z]*）",
+                 "^{}\s?\([^名又a-zA-Z]*\)",
+                 "^{}，[^a-zA-Z]+人"
                  ]
 
 dataDIR = "../data/zhwiki_abstract_2306"
@@ -16,17 +18,28 @@ def main(entryDIR):
     """
     分辨 fileLIST 中的每一個 json 檔，看它是不是屬於「人類」。如果是的話，就加入列表 [PersonLIST] 中
     """
+    personLIST = []
     for json_f in os.listdir(entryDIR):
-        with open("{}/{}".format(entryDIR, json_f), encoding="utf-8") as f:
-            topicSTR = json_f.replace(".json", "")
-            entrySTR = json.load(f)["abstract"]
-
-    return None
+        try:
+            with open("{}/{}".format(entryDIR, json_f), encoding="utf-8") as f:
+                topicSTR = json_f.replace(".json", "")
+                entrySTR = json.load(f)["abstract"]
+            for p in personPatLIST:
+                pat = re.compile(p.format(topicSTR))
+                if len(list(re.finditer(pat, entrySTR))) > 0:
+                    personLIST.append(topicSTR)
+                else:
+                    pass
+        except IsADirectoryError:
+            pass
+    return personLIST
 
 
 if __name__ == "__main__":
+    personLIST = []
     for init_s in os.listdir(dataDIR):
         if init_s.startswith("._"):
             pass
         else:
-            main("{}/{}".format(dataDIR, init_s))
+            personLIST.extend(main("{}/{}".format(dataDIR, init_s)))
+    pprint(personLIST)
