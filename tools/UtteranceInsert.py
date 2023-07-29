f059707233120200474897b48d09978fb0e70361
PeterWolf <peterwolf.wang@gmail.com>
2023-07-29 16:56:42 +0800

    依 entity 數量分類意圖

--- tools/UtteranceInsert.py
+++ tools/UtteranceInsert.py
@@ -11,23 +11,38 @@ projectName = "AItlas_Wiki_Demo"
 targetVerb = "代表"
 purgePat = re.compile("</?[a-zA-Z]+(_[a-zA-Z]+)?>")
 
+countLIST = ["KNOWLEDGE_", "ENTITY_noun", "ENTITY_oov", "ENTITY_person", "LOCATION", "UserDefined"]
+
 with open("./account.info", encoding="utf-8") as f:
     accountDICT = json.load(f)
 
 articut = Articut(username=accountDICT["username"], apikey=accountDICT["apikey"])
 
 def verbEntityChoper(inputLIST, targetVerb, purgePat):
-    resultLIST = []
+    resultDICT = {}
     for i  in inputLIST:
-        resultDICT = articut.parse(i)
-        verbLIST = articut.getVerbStemLIST(resultDICT)
+        articutResultDICT = articut.parse(i)
+        verbLIST = articut.getVerbStemLIST(articutResultDICT)
         for v in verbLIST[0]:
             if v[-1] == targetVerb:
-                resultLIST.append(re.sub(purgePat, "", resultDICT["result_pos"][0][v[0]:]))
-    resultLIST = list(set(resultLIST)-set([targetVerb]))
-    return resultLIST
+                entityNum = 0
+                for e in countLIST:
+                    entityNum = entityNum + int((articutResultDICT["result_pos"][0][v[0]:].count(e))/2)
+                    if entityNum == 0:
+                        pass
+                    elif entityNum in resultDICT:
+                        pass
+                    else:
+                        resultDICT[entityNum] = []
+                if entityNum == 0:
+                    pass
+                else:
+                    resultDICT[entityNum].append(re.sub(purgePat, "", articutResultDICT["result_pos"][0][v[0]:]))
+    for k in resultDICT.keys():
+        resultDICT[k] = list(set(resultDICT[k]))
+    return resultDICT
 
-def createIntent(username, loki_key, targetVerb):
+def createIntent(username, loki_key, targetVerb, entityNum):
     global projectName
     intentName = "".join(articut.parse(targetVerb, level="lv3", pinyin="HANYU")["utterance"][0]).replace(" ", "")
 
@@ -36,7 +51,7 @@ def createIntent(username, loki_key, targetVerb):
         "username" : username,
         "loki_key" : loki_key,
         "project": projectName,
-        "intent": intentName,
+        "intent": intentName + "_{}e".format(entityNum),
         "func": "create_intent",
         "data": {
             "type": "basic" #意圖類別
@@ -46,34 +61,36 @@ def createIntent(username, loki_key, targetVerb):
     response = post(url, json=payload).json()
     return (intentName, response)
 
-def insertUtterance(username, loki_key, targetVerb, utterance2AddLIST):
+def insertUtterance(username, loki_key, targetVerb, utterance2AddDICT):
     global projectName
     intentName = "".join(articut.parse(targetVerb, level="lv3", pinyin="HANYU")["utterance"][0]).replace(" ", "")
 
     url = "https://api.droidtown.co/Loki/Call/"
     batchSize = 20
-    for i in range(0, len(utterance2AddLIST), batchSize):
-        payload = {
-            "username" : username,
-            "loki_key" : loki_key,
-            "project": projectName,
-            "intent": intentName,
-            "func": "insert_utterance",
-            "data": {
-                "utterance": utterance2AddLIST[i:i+batchSize],
-                "checked_list": []
+    response = None
+    for k in utterance2AddDICT.keys():
+        createIntent(accountDICT["username"], accountDICT["loki_key"], targetVerb, k)
+        for i in range(0, len(utterance2AddDICT[k]), batchSize):
+            print(utterance2AddDICT[k][i:i+batchSize])
+            payload = {
+                "username" : username,
+                "loki_key" : loki_key,
+                "project": projectName,
+                "intent": intentName+"_{}e".format(k),
+                "func": "insert_utterance",
+                "data": {
+                    "utterance": utterance2AddDICT[k][i:i+batchSize],
+                    "checked_list": ["LOCATION", "ENTITY_noun"]
+                }
             }
-        }
-        response = post(url, json=payload).json()
+            response = post(url, json=payload).json()
     return response
 
 
 if __name__ == "__main__":
-    intentCreateResult = createIntent(accountDICT["username"], accountDICT["loki_key"], targetVerb)
-    print(intentCreateResult)
 
     cadidateLIST = []
-    for dir_s in os.listdir("../People_Source"):
+    for dir_s in os.listdir("../People_Source")[:100]:
         if dir_s == ".DS_Store":
             os.remove(dir_s)
         elif dir_s.startswith("."):
@@ -96,8 +113,8 @@ if __name__ == "__main__":
 
 
     print("建立意圖..", cadidateLIST)
-    utterance2AddLIST = verbEntityChoper(cadidateLIST, targetVerb, purgePat)
-    print(utterance2AddLIST)
+    utterance2AddDICT = verbEntityChoper(cadidateLIST, targetVerb, purgePat)
+    print(utterance2AddDICT)
 
-    utteranceInsertResult = insertUtterance(accountDICT["username"], accountDICT["loki_key"], targetVerb, utterance2AddLIST)
+    utteranceInsertResult = insertUtterance(accountDICT["username"], accountDICT["loki_key"], targetVerb, utterance2AddDICT)
     print("新增語句...", utteranceInsertResult)
