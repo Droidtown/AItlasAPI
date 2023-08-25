5e9042ca12200e4daa507c820e78c536a9a58ff9
Cathy <99934754+shendongCathy@users.noreply.github.com>
2023-08-25 11:04:57 +0800

    Update verbs_utterances.py
    fix 'str' problem

--- tools/verbs_utterances.py
+++ tools/verbs_utterances.py
@@ -36,10 +36,12 @@ def verbExtractor(VERB):
 def Noun_utt(entities, result_pos):
     result=[]
     for i, e in enumerate(entities):      
-        utt = ''.join(re.findall(r'[一-龥]*', result_pos[i]))   
+        # print("第{}個句子，名詞是{}]".format(i,e))
+        utt = ''.join(re.findall(r'[一-龥]*', result_pos[i]))  #wash the details 
+        # print("有{}個的名詞句子:{}".format(len(e),utt)) 
         result.append(utt) 
     result=[u for u in result if u!='']
-    return result      
+    return result      #名詞的句子
 
 def create_Vjson(VerbLIST): 
     for verbs in VerbLIST:
@@ -71,14 +73,15 @@ def main(folder_path,s):
                 else:
                     with open("../data/People_Source230730/{}/{}".format(dir_s, j_file), encoding="utf-8") as f:
                         data = json.load(f)
-                        verb_data = data["abstract"] 
+                        verb_data = data["abstract"] #要parse的句子
                         resultDICT = articut.parse(verb_data, level="lv1") 
+                        #result_pos = resultDICT['result_pos']
                         verb = articut.getVerbStemLIST(resultDICT)  #get verbs with index      
-                        VerbLIST=verbExtractor(verb) #get only verbs 
+                        VerbLIST=set(verbExtractor(verb)) #get only verbs 
                         create_Vjson(VerbLIST) #create  V.json files
                         entities=articut.getNounStemLIST(resultDICT) #get entities with index
-                        result_pos=resultDICT['result_pos']#get result_pos 
-                        result = Noun_utt(entities,result_pos) # utterances with the intersection of verbs and entities
+                        result_pos=resultDICT['result_pos']
+                        result = Noun_utt(entities,result_pos) # utterance with the intersection of verbs and entities
                         #utt_result=[]
                         relay_time= 1.6
                         for V in VerbLIST: 
@@ -89,25 +92,25 @@ def main(folder_path,s):
                                     result_pos1=results['result_pos']
                                     new_data={}
                                     for i, e in enumerate(entities_result): 
+                                        # print("第{}個句子，名詞是{}，動詞是{}]".format(i+1,e,V))
                                         utt = ''.join(re.findall(r'[一-龥]*', result_pos1[i]))  #wash the details 
                                         n = len(e)
+                                        # print("有{}個的名詞句子:{}".format(n,utt)) 
                                         new_data[f"{n}e"] = [utt]
                                         filename = f"{V}.json"
                                         target_path = os.path.join(target_folder, filename)
                                         with open(target_path, "r") as f:
                                             existing_data = json.load(f)
                                             for key, value in new_data.items():
-                                                existing_data.setdefault(key, [])
+                                                existing_data.setdefault(key, []) #處理同一個數量的entity的utterance加入，str不能append的問題
                                                 for item in value:
-                                                    if item in existing_data[key]:
-                                                        print("{}already existed in:{}".format(new_data,filename))
-                                                    else:
+                                                    if item not in existing_data[key]:
                                                         existing_data[key].append(item)
                                                         with open(target_path, 'w', encoding='utf-8') as file:
                                                             json.dump(existing_data, file, ensure_ascii=False, indent=4)
                                                         print("{}sucessfully saved in :{}".format(new_data,filename))
-                                                    
-                                                                                                                                                                                   
+                                                    else:
+                                                        print("{}already existed in:{}".format(new_data,filename))                                                     
                                 else: 
                                     pass
                             time.sleep(relay_time)                        
