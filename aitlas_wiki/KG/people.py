75ea9c3333bdc36a6707ca4bf930a99d5b900fb7
Peter <peter.w@droidtown.co>
2023-09-07 20:59:30 +0800

    Change data structure to fit new protocol.

--- aitlas_wiki/KG/people.py
+++ aitlas_wiki/KG/people.py
@@ -2,21 +2,27 @@
 # -*- coding:utf-8 -*-
 
 schema = {"verb_01":{"attribute":[],
-                     "entity_obj":["entity_subj01", "entity_02"]
+                     "entity_obj":[{"name":"entity_subj01", "time":["entity_02"]}]
                      },
           "verb_02":{"attribute":[],
-                     "entity_obj":["entity_subj01", "entity_02"]
+                     "entity_obj":[{"name":"entity_subj01", "time":["entity_02"]}
+                                  ]
                      },
           }
 
 action = {"代表":{"attribute":[],
-                 "日本":[["末綱聰子", ["2008"]], ["武士", []]],
-                 "德國":[["朱利安·申克", ["2008"]]]
+                 "日本":[{"name":"末綱聰子", "time":["2008"]},
+                        {"name":"武士", "time":[]}
+                        ],
+                 "德國":[{"name":"朱利安·申克", "time":["2008"]}
+                        ]
                 },
           "參加":{"attribute":[],
-                 "奧林匹克":[["末綱聰子", ["2008"]]],
+                 "奧林匹克":[{"name":"末綱聰子", "time":["2008"]}
+                           ],
                   },
           "出戰":{"attribute":[],
-                 "北京奧會":[["朱利安·申克", ["2008"]]]
+                 "北京奧會":[{"name":"朱利安·申克", "time":["2008"]}
+                           ]
                 }
           }
