61feb0bea4ee873485ebed275a7fbb347a1ef098
PeterWolf <peterwolf.wang@gmail.com>
2024-01-16 15:31:54 +0800

    更新[Person]的濾出程式

--- tools/person_classifier.py
+++ tools/person_classifier.py
@@ -4,70 +4,47 @@
 import json
 import os
 import re
-import shutil
-#from pprint import pprint
+
+purgedPat = re.compile("[\(（].*?[\)）]")
+
+personPat = re.compile("[^a-zA-Z\d或論敵險事繫食示]+([人士員]|出身|學家)$|(?<=[，,。；？！、])[^a-zA-Z\d或論敵險事繫食示，。？！；、]+([人士員]|出身|學家)(?=[、，。])")
 
 
-personPatLIST = ["^{}\s?（[^名又a-zA-Z]+）",
-                 "^{}\s?\([^名又a-zA-Z]+\)",
-                 "^{}\s?（[^名又a-zA-Z]*）",
-                 "^{}，[^a-zA-Z]+人\b",
-                 "^{}，[^a-zA-Z]+人(?=[，。])"
-                ]
 
-
-dataDIR = "../data/zhwiki_abstract_2306"
+dataDIR = "../data/local_data/person"
 destination_dir = "../data/People/{}"
 
-def main(entryDIR):
-
-    personLIST = []
-    for json_f in os.listdir(entryDIR):
+def removalListMaker(entryDIR):
+    removeLIST = []
+    fileLIST = os.listdir("{}/{}".format(dataDIR, entryDIR))
+    for f in fileLIST:
         try:
-            with open("{}/{}".format(entryDIR, json_f), encoding="utf-8") as f:
-                topicSTR = json_f.replace(".json", "")
-                entrySTR = json.load(f)["abstract"]
-            for p in personPatLIST:
-                pat = re.compile(p.format(topicSTR))
-                if len(list(re.finditer(pat, entrySTR))) > 0:
-                    personLIST.append(topicSTR)
-                else:
-                    pass
+            jContent = json.load(open(f"{dataDIR}/{entryDIR}/{f}"))["abstract"]
+            jContent = purgedPat.sub("", jContent)
+            if list(personPat.finditer(jContent)) == []:
+                removeLIST.append(f)
+            else:
+                pass
         except:
-            pass
-    return personLIST
+            removeLIST.append(f)
+
+    return list(set(removeLIST))
 
 if __name__ == "__main__":
     personLIST = []
-    data_files = os.listdir(dataDIR)
-    num_files = len(data_files)
+    data_dirs = os.listdir(dataDIR)
+    num_files = len(data_dirs)
     batch_size = 10
 
-    for i in range(0, num_files, batch_size):
-        batch_files = data_files[i:i+batch_size]
-
-        for init_s in batch_files:
-            if init_s.startswith("."):
-                continue
+    for e_dir in data_dirs:
+        removeLIST = removalListMaker(e_dir)
+        for r in removeLIST:
+            if r.startswith("."):
+                try:
+                    os.remove(f"{dataDIR}/{e_dir}/{r}")
+                except:
+                    pass
             else:
-                personLIST.extend(main("{}/{}".format(dataDIR, init_s)))
-
-    for root, dirs, files in os.walk(dataDIR):
-        for file in files:
-            if file.endswith(".json"):
-                for member in personLIST:
-                    if file.replace(".json", "") == member:
-                        source_path = os.path.join(root, file)
-
-                        if os.path.exists(destination_dir.format(file[0])):
-                            pass
-                        else:
-                            os.makedirs(destination_dir.format(file[0]))
-
-                        destination_path = os.path.join(destination_dir.format(file[0]), file)
-                        shutil.copy(source_path, destination_path)
-                        print(f"檔案 {file} 已成功複製到 {destination_path} 資料夾。")
-
-
-
-    print("Finished processing part of the data files.")
+                os.remove(f"{dataDIR}/{e_dir}/{r}")
+        if os.listdir(f"{dataDIR}/{e_dir}") == []:
+            os.removedirs(f"{dataDIR}/{e_dir}")
