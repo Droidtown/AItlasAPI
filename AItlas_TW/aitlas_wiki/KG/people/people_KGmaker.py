4ebc9e9e77095fed5b14235b7fe0ed92c8553c23
PeterWolf <peterwolf.wang@gmail.com>
2024-01-18 20:59:53 +0800

    測試 typing 標記

--- AItlas_TW/aitlas_wiki/KG/people/people_KGmaker.py
+++ AItlas_TW/aitlas_wiki/KG/people/people_KGmaker.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-def main():
-    """"""
+def typeLIST():
+
+
     return None
 
 
--- AItlas_TW/aitlas_wiki/KG/people/people_type.py
+++ AItlas_TW/aitlas_wiki/KG/people/people_type.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+
 Noun = str
 
 from dataclasses import dataclass
