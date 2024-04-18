32c204b3acfc40ac3ac485bfd855e3f596466ae3
byen2 <13092088+byen2@users.noreply.github.com>
2024-04-18 02:50:04 -0700

    rename preprocess file

--- AItlas_EN/preprocess_EN.py
+++ AItlas_EN/preprocess_EN.py
@@ -19,21 +19,21 @@ G_basePath = os.path.dirname(os.path.abspath(__file__))
 
 G_url = "http://127.0.0.1:30100"
 
-def runEthik(inputSTR, level="lv1", userDefinedDICT={}):
+def runArticutEN(inputSTR, level="lv1", userDefinedDICT={}):
     payload = {"input_str": inputSTR,
                "level": level,
                "user_defined_dict_file": userDefinedDICT}
-    respond = requests.post("{}/Ethik/API/".format(G_url), json=payload)
+    respond = requests.post("{}/ArticutEN/API/".format(G_url), json=payload)
     resultDICT = respond.json()
     print(respond)
     #pprint(resultDICT)
     return resultDICT
 
-def runEthikBulk(inputLIST, level="lv1", userDefinedDICT={}):
+def runArticutENBulk(inputLIST, level="lv1", userDefinedDICT={}):
     payload = {"input_list": inputLIST,
                "level": level,
                "user_defined_dict_file": userDefinedDICT}
-    respond = requests.post("{}/Ethik/BulkAPI/".format(G_url), json=payload)
+    respond = requests.post("{}/ArticutEN/BulkAPI/".format(G_url), json=payload)
     resultDICT = respond.json()
     print(respond)
     #pprint(resultDICT)
@@ -98,11 +98,11 @@ def splitBulkSentences(directory_path: str):
 if __name__ == "__main__":
     inputLIST = ["A farmer in Australia has just entered the record books for growing the world's biggest ever blueberry.",
                  "The giant berry weighed in at a mammoth 20.4 grams."]
-    print("runEthik()")
-    pprint(runEthik(inputLIST[0]))
+    print("runArticutEN()")
+    pprint(runArticutEN(inputLIST[0]))
 
-    print("runEthikBulk()")
-    pprint(runEthikBulk(inputLIST))
+    print("runArticutENBulk()")
+    pprint(runArticutENBulk(inputLIST))
 
     #testing split sentences
     '''input = "Augusta Ada King, Countess of Lovelace (n\u00e9e Byron; 10 December 1815 \u2013 27 November 1852) was an English mathematician and writer, chiefly known for her work on Charles Babbage's proposed mechanical general-purpose computer, the Analytical Engine. She was the first to recognise that the machine had applications beyond pure calculation, and to have published the first algorithm intended to be carried out by such a machine. As a result, she is often regarded as the first computer programmer.Ada Byron was the only legitimate child of poet Lord Byron and Lady Byron. All of Byron's other children were born out of wedlock to other women. Byron separated from his wife a month after Ada was born and left England forever. Four months later, he commemorated the parting in a poem that begins, \"Is thy face like thy mother's my fair child! ADA! sole daughter of my house and heart?\" He died in Greece when Ada was eight. Her mother remained bitter and promoted Ada's interest in mathematics and logic in an effort to prevent her from developing her father's perceived insanity. Despite this, Ada remained interested in him, naming her two sons Byron and Gordon. Upon her death, she was buried next to him at her request. Although often ill in her childhood, Ada pursued her studies assiduously. She married William King in 1835. King was made Earl of Lovelace in 1838, Ada thereby becoming Countess of Lovelace.\nHer educational and social exploits brought her into contact with scientists such as Andrew Crosse, Charles Babbage, Sir David Brewster, Charles Wheatstone, Michael Faraday, and the author Charles Dickens, contacts which she used to further her education. Ada described her approach as \"poetical science\" and herself as an \"Analyst (& Metaphysician)\".When she was eighteen, her mathematical talents led her to a long working relationship and friendship with fellow British mathematician Charles Babbage, who is known as \"the father of computers\". She was in particular interested in Babbage's work on the Analytical Engine. Lovelace first met him in June 1833, through their mutual friend, and her private tutor, Mary Somerville.\nBetween 1842 and 1843, Ada translated an article by Italian military engineer Luigi Menabrea about the Analytical Engine, supplementing it with an elaborate set of notes, simply called \"Notes\". Lovelace's notes are important in the early history of computers, containing what many consider to be the first computer program\u2014that is, an algorithm designed to be carried out by a machine. Other historians reject this perspective and point out that Babbage's personal notes from the years 1836/1837 contain the first programs for the engine. She also developed a vision of the capability of computers to go beyond mere calculating or number-crunching, while many others, including Babbage himself, focused only on those capabilities. Her mindset of \"poetical science\" led her to ask questions about the Analytical Engine (as shown in her notes) examining how individuals and society relate to technology as a collaborative tool."
@@ -112,7 +112,7 @@ if __name__ == "__main__":
     #the plan:
     #go through each json file in people directory
     #for each file, take the contents after "summary", run it through split_sentences, returning list of str
-    #run list of str through EthikBulk
+    #run list of str through ArticutENBulk
 
     #test splitBulkSentences functionality
     '''test_folder = os.path.dirname(os.path.abspath(__file__))
