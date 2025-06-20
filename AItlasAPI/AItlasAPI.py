1ef31e37009009bcfc333126e337e190faceacb4
cynthiaYun <cynthia.l@droidtown.co>
2025-06-20 17:06:34 +0800

    將`createKG`中`KG_FilePath`參數改回預設的樣子

--- AItlasAPI/AItlasAPI.py
+++ AItlasAPI/AItlasAPI.py
@@ -454,7 +454,7 @@ if __name__ == "__main__":
     #entity = "前田美順"
     #utteranceLIST = ["末綱聰子與前田美順的組合代表日本參加北京舉行的奧運會羽球女子雙打比賽"]
     aitlas = AItlas()
-    #aitlas.createKG(inputSTR=longText, KG_FilePath= kgDIR/ "aitlas.kg")
+    #aitlas.createKG(inputSTR=longText, KG_FilePath= "aitlas.kg")
 
     longText = """民眾黨前主席柯文哲的父親柯承發今天辭世。民眾黨代理黨主席黃國昌說，請柯家人放心，民眾黨會做他們最堅強的後盾；所有後事，都要尊重柯家人的意願跟想法，希望能尊重他們的隱私。
 國民黨主席朱立倫也透過聲明表示，對於柯文哲的父親柯承發過世，深表哀悼，希望柯文哲以及其家人節哀珍重。"""
